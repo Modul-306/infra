b938f356969fc1bc97354e2a6b17beb14c07cd4d
Fabio Flückiger <ffl140508@stud.gibb.ch>
2025-01-31 18:09:27 +0100

    fix: terraform pipeline

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "Module-306"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Module-306-infra"
     }
   }
 }
