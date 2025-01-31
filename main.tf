cb11e88640076f5c5edd976ffb468892ac6934f3
Fabio Flückiger <ffl140508@stud.gibb.ch>
2025-01-31 18:25:26 +0100

    feat: terraform fmt

--- main.tf
+++ main.tf
@@ -63,9 +63,9 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
