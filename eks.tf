1df818c4b1210b46d021a698970b70e24b52ddfe
Fabio Flückiger <ffl140508@stud.gibb.ch>
2025-04-22 16:22:41 +0200

    feat: infra

---
+++ eks.tf
@@ -0,0 +1,40 @@
+resource "aws_eks_cluster" "m306" {
+  name = "eks-m306"
+
+  role_arn = data.aws_iam_role.labrole.arn
+  version  = "1.31"
+
+  compute_config {
+    enabled       = true
+    node_pools    = ["general-purpose"]
+    node_role_arn = data.aws_iam_role.labrole.arn
+  }
+
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = true
+    }
+  }
+
+  storage_config {
+    block_storage {
+      enabled = true
+    }
+  }
+
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access  = true
+
+    subnet_ids = [
+      aws_subnet.subnet_1.id,
+      aws_subnet.subnet_2.id,
+      aws_subnet.subnet_3.id
+    ]
+  }
+}
+
+# ECR Repository
+resource "aws_ecr_repository" "example" {
+    name = "repo-m306"
+}
---
+++ network.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "network" {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags = {
+        Name = "m306-vpc"
+    }
+}
+
+resource "aws_subnet" "subnet_1" {
+    vpc_id            = aws_vpc.network.id
+    cidr_block        = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "m306-subnet-1"
+    }
+}
+
+resource "aws_subnet" "subnet_2" {
+    vpc_id            = aws_vpc.network.id
+    cidr_block        = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "m306-subnet-2"
+    }
+}
+
+resource "aws_subnet" "subnet_3" {
+    vpc_id            = aws_vpc.network.id
+    cidr_block        = "10.0.3.0/24"
+    availability_zone = "us-east-1c"
+
+    tags = {
+        Name = "m306-subnet-3"
+    }
+}
