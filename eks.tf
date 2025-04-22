adef5713a3bb5d47375e80e45e316a305cb60ca0
Fabio Flückiger <ffl140508@stud.gibb.ch>
2025-04-22 16:30:56 +0200

    feat: infra

--- eks.tf
+++ eks.tf
@@ -1,37 +1,41 @@
 resource "aws_eks_cluster" "m306" {
-  name = "eks-m306"
-
+  name     = "eks-m306"
   role_arn = data.aws_iam_role.labrole.arn
   version  = "1.31"
-
-  compute_config {
-    enabled       = true
-    node_pools    = ["general-purpose"]
-    node_role_arn = data.aws_iam_role.labrole.arn
-  }
-
-  kubernetes_network_config {
-    elastic_load_balancing {
-      enabled = true
-    }
-  }
-
-  storage_config {
-    block_storage {
-      enabled = true
-    }
-  }
 
   vpc_config {
     endpoint_private_access = true
     endpoint_public_access  = true
-
     subnet_ids = [
       aws_subnet.subnet_1.id,
       aws_subnet.subnet_2.id,
       aws_subnet.subnet_3.id
     ]
   }
+
+  depends_on = [
+    data.aws_iam_role.labrole
+  ]
+}
+
+# Node Group for EKS
+resource "aws_eks_node_group" "m306" {
+  cluster_name    = aws_eks_cluster.m306.name
+  node_group_name = "m306-node-group"
+  node_role_arn   = data.aws_iam_role.labrole.arn
+  subnet_ids      = [
+    aws_subnet.subnet_1.id,
+    aws_subnet.subnet_2.id,
+    aws_subnet.subnet_3.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
 }
 
 # ECR Repository
