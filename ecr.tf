db6dc0bb6b066beac49c9985baba78f978d71358
Fabio Flückiger <ffl140508@stud.gibb.ch>
2025-05-13 16:04:34 +0200

    chore: add helm repos

--- ecr.tf
+++ ecr.tf
@@ -14,3 +14,20 @@ resource "aws_ecr_repository" "frontend_staging" {
 resource "aws_ecr_repository" "frontend_prod" {
   name = "prod-m306-frontend"
 }
+
+# Helm Repositories
+resource "aws_ecr_repository" "frontend_helm_prod" {
+  name = "prod-m306-helm-frontend"
+}
+
+resource "aws_ecr_repository" "frontend_helm_staging" {
+  name = "staging-m306-helm-frontend"
+}
+
+resource "aws_ecr_repository" "backend_helm_prod" {
+  name = "prod-m306-helm-backend"
+}
+
+resource "aws_ecr_repository" "backend_helm_staging" {
+  name = "staging-m306-helm-backend"
+}
