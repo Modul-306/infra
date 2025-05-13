4b9ae8b26b73174298002dd00f22188848acc1de
dudeand <113430070+dudeand@users.noreply.github.com>
2025-05-13 14:29:26 +0000

    terraform-fmt: automated action

--- ecr.tf
+++ ecr.tf
@@ -1,49 +1,49 @@
 # ECR Repositories
 resource "aws_ecr_repository" "backend_staging" {
-  name = "staging-m306-backend"
+  name                 = "staging-m306-backend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "backend_prod" {
-  name = "prod-m306-backend"
+  name                 = "prod-m306-backend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "frontend_staging" {
-  name = "staging-m306-frontend"
+  name                 = "staging-m306-frontend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "frontend_prod" {
-  name = "prod-m306-frontend"
+  name                 = "prod-m306-frontend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 # Helm Repositories
 resource "aws_ecr_repository" "frontend_helm_prod" {
-  name = "prod-m306-helm-frontend"
+  name                 = "prod-m306-helm-frontend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "frontend_helm_staging" {
-  name = "staging-m306-helm-frontend"
+  name                 = "staging-m306-helm-frontend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "backend_helm_prod" {
-  name = "prod-m306-helm-backend"
+  name                 = "prod-m306-helm-backend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "backend_helm_staging" {
-  name = "staging-m306-helm-backend"
+  name                 = "staging-m306-helm-backend"
   image_tag_mutability = "MUTABLE"
-  force_delete = true
+  force_delete         = true
 }
