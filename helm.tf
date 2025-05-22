623d4ba29fcb81c8960d7535e1b9c3caf128f011
Fankhauserli <128742858+Fankhauserli@users.noreply.github.com>
2025-05-22 11:52:07 +0000

    terraform-fmt: automated action

--- helm.tf
+++ helm.tf
@@ -20,9 +20,9 @@ resource "helm_release" "prod-backend" {
   name       = "prod-m306-helm-backend"
   repository = "oci://${aws_ecr_repository.backend_helm_prod.registry_id}.dkr.ecr.us-east-1.amazonaws.com"
 
-  chart      = "prod-m306-helm-backend"
-  version    = "0.1.1"
-  namespace  = "m306"
+  chart     = "prod-m306-helm-backend"
+  version   = "0.1.1"
+  namespace = "m306"
 
   # Ensure ECR authentication is properly configured
   repository_username = data.aws_ecr_authorization_token.token.user_name
