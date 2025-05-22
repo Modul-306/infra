3bdb563392bd71b03b42961b09081f417e5f1da1
Linus Fankhauser <linus.fankhauser@proton.me>
2025-05-22 13:50:42 +0200

    chore: rebase

--- provider.tf
+++ provider.tf
@@ -26,7 +26,11 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.m306.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.m306.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.m306.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.m306.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.m306.name]
+      command     = "aws"
+    }
   }
 }
