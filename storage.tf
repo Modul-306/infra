5b087d96179e3587d8ab3c2d1c8e901df9ae896a
Linus Fankhauser <linus.fankhauser@proton.me>
2025-05-22 11:11:09 +0200

    feat: add IAM role association for RDS instance with S3 integration

--- storage.tf
+++ storage.tf
@@ -10,6 +10,16 @@ resource "aws_db_instance" "m306" {
   skip_final_snapshot  = true
 
   db_name = "m306db"
+
+  # Add IAM role association
+  iam_database_authentication_enabled = true
+}
+
+# Associate IAM role with RDS instance
+resource "aws_db_instance_role_association" "m306_role" {
+  db_instance_identifier = aws_db_instance.m306.id
+  feature_name          = "S3_INTEGRATION"
+  role_arn             = data.aws_iam_role.labrole.arn
 }
 
 # S3 Bucket
