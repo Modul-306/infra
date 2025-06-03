4f0f45e71b6e74777778359d34d8d86c64236c8b
Fabio Flückiger <ffl140508@stud.gibb.ch>
2025-06-03 15:33:25 +0200

    fix: try fixing not working kubernetes

--- provider.tf
+++ provider.tf
@@ -20,7 +20,12 @@ provider "aws" {
 provider "kubernetes" {
   host                   = aws_eks_cluster.m306.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.m306.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.m306.token
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.m306.name]
+  }
 }
 
 provider "helm" {
