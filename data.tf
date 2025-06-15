368d496d418381d2cccfa3f812e86c17bf14f3a7
Linus Fankhauser <linus.fankhauser@proton.me>
2025-06-15 15:37:08 +0200

    Refactor: remove redundant AWS Load Balancer Controller service account and update Helm release version

--- data.tf
+++ data.tf
@@ -3,19 +3,3 @@ data "aws_iam_role" "labrole" {
 }
 
 data "aws_ecr_authorization_token" "token" {}
-
-# Create service account for AWS Load Balancer Controller using existing LabRole
-resource "kubernetes_service_account" "aws_load_balancer_controller" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = data.aws_iam_role.labrole.arn
-    }
-  }
-
-  depends_on = [
-    aws_eks_cluster.m306,
-    aws_eks_node_group.fast_nodes
-  ]
-}
